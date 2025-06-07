2969e69dcc75b5db865dc2c9a858d5a16ebbe24a
Harshal Khairnar <harshalkhairnar7244@gmail.com>
2025-06-07 23:39:41 +0530

    login registration form created

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,4 +1,5 @@
 
 import logo from './logo.png'
+import header from './header.png'
 
-export const assets={logo}
+export const assets={logo,header}
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,107 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import { assets } from "../assets/assets";
+import { FaUser } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import { RiLockPasswordFill } from "react-icons/ri";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  return (
-    <div>
-      <h1>Login Page</h1>
-    </div>
-  )
-}
 
-export default Login
+    const navigate=useNavigate();
+
+  const [state, setState] = useState("Sign Up");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <div className=" flex items-center justify-center min-h-screen sm:px-0 bg-gradient-to-br from-yellow-200 to-pink-400">
+      <img
+      onClick={()=>navigate('/')}
+        src={assets.logo}
+        alt=""
+        className=" absolute left-5 sm:left-20  top-5 w-20 sm:w-20 cursor-pointer"
+      />
+      <div className="   bg-pink-900 p-10  rounded-lg shadow-lg w-full sm:w-96 text-indigo-300  text-sm">
+        <h2 className=" text-3xl font-semibold text-white text-center mb-3">
+          {state === "Sign Up" ? "Create Account" : "Login "}
+        </h2>
+        <p className=" text-center text-sm mb-6">
+          {state === "Sign Up"
+            ? "Create your account"
+            : "Login to your account!"}
+        </p>
+
+        <form action="" className=" text-gray-800">
+          {state === "Sign Up" && (
+            <div className=" mb-4  flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#ad54d6]">
+              <FaUser />
+              <input
+                onChange={(e) => setName(e.target.value)}
+                value={name}
+                type="text"
+                placeholder="Full Name"
+                required
+                className="  text-gray-800 bg-transparent outline-none"
+              />
+            </div>
+          )}
+
+          <div className=" mb-4  flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#ad54d6]">
+            <MdEmail />
+            <input
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              type="email"
+              placeholder="Email Id"
+              required
+              className="  text-gray-800 bg-transparent outline-none"
+            />
+          </div>
+          <div className=" mb-4  flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#ad54d6]">
+            <RiLockPasswordFill />
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              type="text"
+              placeholder="Password"
+              required
+              className="  text-gray-800 bg-transparent outline-none"
+            />
+          </div>
+          <p
+          onClick={()=>navigate('/reset-password')}
+          className="ml-2 mb-4  text-indigo-200  cursor-pointer">
+            Forgot Password?
+          </p>
+          <button className=" w-full py-2.5 rounded-full bg-gradient-to-r from-yellow-500 to-orange-500 text-white font-medium cursor-pointer">
+            {state}
+          </button>
+        </form>
+        {state === "Sign Up" ? (
+          <p className="  text-gray-400 text-center text-xs mt-4">
+            Already have an account?{" "}
+            <span
+              onClick={() => setState("Login")}
+              className=" text-blue-400 cursor-pointer underline"
+            >
+              Login here
+            </span>
+          </p>
+        ) : (
+          <p className="  text-gray-400 text-center text-xs mt-4">
+            Don't have an account?{" "}
+            <span
+              onClick={() => setState("Sign Up")}
+              className=" text-blue-400 cursor-pointer underline"
+            >
+              Sign Up
+            </span>
+          </p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Login;
