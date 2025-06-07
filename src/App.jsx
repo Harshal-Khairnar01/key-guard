5205148b3a5a4a4187706249269dab2c9a586b13
Harshal Khairnar <harshalkhairnar7244@gmail.com>
2025-06-07 19:21:56 +0530

    routes created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 
 import React from 'react'
+import { Routes ,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import ResetPassword from './pages/ResetPassword'
+import EmailVerify from './pages/EmailVerify'
+import Login from './pages/Login'
 
 const App = () => {
   return (
     <div>
-      <h1 className='    text-red-500'>Harshal</h1>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/login' element={<Login/>}></Route>
+        <Route path='/verify-email' element={<EmailVerify/>}></Route>
+        <Route path='/reset-password' element={<ResetPassword/>}></Route>
+      </Routes>
     </div>
   )
 }
