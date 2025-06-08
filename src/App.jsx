4d222f400e46dfb5df461eb5c29609ff59436c5e
Harshal Khairnar <harshalkhairnar7244@gmail.com>
2025-06-08 15:24:48 +0530

    verify otp UI done

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home/>}></Route>
         <Route path='/login' element={<Login/>}></Route>
-        <Route path='/verify-email' element={<EmailVerify/>}></Route>
+        <Route path='/email-verify' element={<EmailVerify/>}></Route>
         <Route path='/reset-password' element={<ResetPassword/>}></Route>
       </Routes>
     </div>
